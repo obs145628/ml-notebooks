88e92d61b1a83c557dc52dc826a654131efb11e8
Steven Lariau <obs145628@gmail.com>
2019-02-25 13:26:33 +0100

    add new conv norm operation

--- docs/utils.py
+++ docs/utils.py
@@ -1,9 +1,12 @@
+import gzip
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import shutil
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import urllib.request
 
 
 _grads = {}
@@ -162,3 +165,44 @@ def plot_reg_contours(X, y, clf, npts=5000):
 
     plt.scatter(X, y, c='b')
     
+
+
+
+
+def load_mnist_np(dir_path):
+    URLS = [
+        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+    ]
+    names = [os.path.basename(x) for x in URLS]
+    out_gzs = [os.path.join(dir_path, x) for x in names]
+    out_files = [x[:-3] for x in out_gzs]
+    os.makedirs(dir_path, exist_ok=True)
+    
+    for i in range(len(URLS)):
+        if not os.path.isfile(out_files[i]):
+            print('Downloading {}...'.format(names[i]))
+            urllib.request.urlretrieve(URLS[i], out_gzs[i])
+            with gzip.open(out_gzs[i], 'rb') as fis:
+                with open(out_files[i], 'wb') as fos:
+                    shutil.copyfileobj(fis, fos)
+                    
+                    
+                    
+    intType = np.dtype('int32').newbyteorder('>')
+    nMetaDataBytes = 4 * intType.itemsize
+    X = []
+    y = []
+    for i in range(2):
+        data = np.fromfile(out_files[2*i], dtype = 'ubyte')
+        magicBytes, nImages, width, height = np.frombuffer(data[:nMetaDataBytes].tobytes(), 
+                                                           intType)
+        data = data[nMetaDataBytes:].astype(dtype='float32').reshape([nImages, width, height])
+        labels = np.fromfile(out_files[2*i+1],
+                             dtype='ubyte')[2*intType.itemsize:]
+        X.append(data)
+        y.append(labels)
+    
+    return X[0], y[0], X[1], y[1]
